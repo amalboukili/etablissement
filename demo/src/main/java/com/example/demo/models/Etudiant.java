faa4decfac7fcd05f40ac3085beaec6619174d1d
amalboukili <boukili.amal@gmail.com>
2019-06-12 13:13:00 +0000

    first commit

---
+++ demo/src/main/java/com/example/demo/models/Etudiant.java
@@ -0,0 +1,27 @@
+package com.example.demo.models;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Table(name = "etudiants")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Etudiant{
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nom;
+    private String prenom;
+    private Date dateNaissance;
+
+}
